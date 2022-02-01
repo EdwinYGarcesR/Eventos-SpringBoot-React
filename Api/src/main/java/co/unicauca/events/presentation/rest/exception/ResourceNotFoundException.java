8774cde49918d447aaddb95bd19b6595c620a987
climacot <fermt111@gmail.com>
2022-02-01 16:02:16 -0500

    serial version a la exepcion not found

--- Api/src/main/java/co/unicauca/events/presentation/rest/exception/ResourceNotFoundException.java
+++ Api/src/main/java/co/unicauca/events/presentation/rest/exception/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package co.unicauca.events.presentation.rest.exception;
 
 public class ResourceNotFoundException extends Exception {
+  private static final long serialVersionUID = 1L;
   public String id;
 
   public ResourceNotFoundException(String id) {
