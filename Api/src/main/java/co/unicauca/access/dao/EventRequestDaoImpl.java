7883a7bf49f7d6c109350a3dff6aeb28afba348e
climacot <fermt111@gmail.com>
2022-02-01 12:30:30 -0500

    impl de dao en blank

---
+++ Api/src/main/java/co/unicauca/access/dao/EventRequestDaoImpl.java
@@ -0,0 +1,23 @@
+package co.unicauca.access.dao;
+
+import java.util.List;
+
+import co.unicauca.domain.model.Event;
+
+public class EventRequestDaoImpl implements IEventRequestDao {
+
+  @Override
+  public List<Event> findAll() {
+    return null;
+  }
+
+  @Override
+  public Event findById(String id) {
+    return null;
+  }
+
+  @Override
+  public Event save(Event eventRequest) {
+    return null;
+  }
+}
