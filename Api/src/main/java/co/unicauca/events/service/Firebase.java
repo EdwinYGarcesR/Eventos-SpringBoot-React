aa6d1a800a2a0564878a6a765e800818a34a28d0
climacot <fermt111@gmail.com>
2022-02-01 11:10:16 -0500

    listo inicalizacion del SDK

---
+++ Api/src/main/java/co/unicauca/events/service/Firebase.java
@@ -0,0 +1,28 @@
+package co.unicauca.events.service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class Firebase {
+  public static String FILE_SERVICE_ACCOUNT_NAME = "src/main/resources/eventos-unicauca-808b2-firebase-adminsdk-jz8s6-6de7fdb3ff.json";
+
+  public static void initializeFirebase() {
+    try (
+        // Use a service account
+        InputStream serviceAccount = new FileInputStream(FILE_SERVICE_ACCOUNT_NAME)) {
+      GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+      FirebaseOptions options = new FirebaseOptions.Builder()
+          .setCredentials(credentials)
+          .build();
+      FirebaseApp.initializeApp(options);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
