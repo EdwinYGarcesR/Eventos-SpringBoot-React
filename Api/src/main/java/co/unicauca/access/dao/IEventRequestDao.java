908844b4f215eb6f5955f5874005206a2b704379
climacot <fermt111@gmail.com>
2022-02-01 12:55:36 -0500

    add throw declaration

--- Api/src/main/java/co/unicauca/access/dao/IEventRequestDao.java
+++ Api/src/main/java/co/unicauca/access/dao/IEventRequestDao.java
@@ -1,12 +1,13 @@
 package co.unicauca.access.dao;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import co.unicauca.domain.model.Event;
 
 public interface IEventRequestDao {
 
-  public List<Event> findAll();
+  public List<Event> findAll() throws InterruptedException, ExecutionException;
 
   public Event findById(String id);
 
