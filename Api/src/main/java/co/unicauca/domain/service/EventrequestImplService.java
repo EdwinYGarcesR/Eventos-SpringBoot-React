8f88d77c5f764037fdf6848e3b0315832e00325c
climacot <fermt111@gmail.com>
2022-02-01 10:49:54 -0500

    create impl de event request service

---
+++ Api/src/main/java/co/unicauca/domain/service/EventrequestImplService.java
@@ -0,0 +1,36 @@
+package co.unicauca.domain.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import co.unicauca.domain.model.Event;
+
+@Service
+public class EventrequestImplService implements IEventRequestService {
+
+  @Override
+  public Event create(Event eventRequest) {
+    return null;
+  }
+
+  @Override
+  public Event findById(String idRequest) {
+    return null;
+  }
+
+  @Override
+  public List<Event> findAll() {
+    return null;
+  }
+
+  @Override
+  public Event update() {
+    return null;
+  }
+
+  @Override
+  public Event delete() {
+    return null;
+  }
+}
