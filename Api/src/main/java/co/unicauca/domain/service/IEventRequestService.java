736f6ae44b34ee846037a2d6a7696b5fccf132b3
climacot <fermt111@gmail.com>
2022-02-01 13:23:11 -0500

    add throw declarations

--- Api/src/main/java/co/unicauca/domain/service/IEventRequestService.java
+++ Api/src/main/java/co/unicauca/domain/service/IEventRequestService.java
@@ -3,9 +3,12 @@ package co.unicauca.domain.service;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import com.google.firebase.auth.FirebaseAuthException;
+
 import co.unicauca.domain.model.Event;
 import co.unicauca.presentation.rest.exception.EventErrorDomainException;
 import co.unicauca.presentation.rest.exception.ResourceNotFoundException;
+import co.unicauca.presentation.rest.exception.TokenErrorInvalid;
 
 /**
  * Interfaz de implementacion de metodos de CRUD para la clase Event
@@ -16,16 +19,20 @@ public interface IEventRequestService {
    * 
    * @param eventRequest Event
    * @return Event creado
+   * @throws TokenErrorInvalid
+   * @throws FirebaseAuthException
    */
-  public Event create(Event eventRequest) throws EventErrorDomainException;
+  public Event create(Event eventRequest, String token) throws EventErrorDomainException, TokenErrorInvalid, FirebaseAuthException;
 
   /**
    * Encontrar un evento por su id
    * 
    * @param idRequest Id del evento
    * @return Event enocntrado
+   * @throws ExecutionException
+   * @throws InterruptedException
    */
-  public Event findById(String idRequest) throws ResourceNotFoundException;
+  public Event findById(String idRequest) throws ResourceNotFoundException, InterruptedException, ExecutionException;
 
   /**
    * Listar los eventos disponibles
