66926f01f5f89309f942e750520b8e6c586a15f1
climacot <fermt111@gmail.com>
2022-02-01 12:27:44 -0500

    full dao impl

--- Api/src/main/java/co/unicauca/access/dao/IEventRequestDao.java
+++ Api/src/main/java/co/unicauca/access/dao/IEventRequestDao.java
@@ -1,5 +1,15 @@
 package co.unicauca.access.dao;
 
+import java.util.List;
+
+import co.unicauca.domain.model.Event;
+
 public interface IEventRequestDao {
-  
+
+  public List<Event> findAll();
+
+  public Event findById(String id);
+
+  public Event save(Event eventRequest);
+
 }
