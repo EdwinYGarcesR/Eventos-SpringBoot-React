02220d1e5e08e67cc65810eaea33b8c835ed98f0
climacot <fermt111@gmail.com>
2022-02-01 14:34:07 -0500

    create eventrequets error  contructor

--- Api/src/main/java/co/unicauca/presentation/rest/exception/EventRequestError.java
+++ Api/src/main/java/co/unicauca/presentation/rest/exception/EventRequestError.java
@@ -4,4 +4,10 @@ public class EventRequestError {
   public EnumErrorCode code;
   public String field;
   public String message;
+
+  public EventRequestError(EnumErrorCode code, String field, String message) {
+    this.code = code;
+    this.field = field;
+    this.message = message;
+  }
 }
